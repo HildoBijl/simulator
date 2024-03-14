c47ce55b8ff0f8f5b9274dd0da26c7063101deb4
Hildo Bijl <hildobijl@gmail.com>
2024-03-14 10:52:09 +0100

    Add userData firestore collection with corresponding hooks

--- src/firebase/auth/hooks.js
+++ src/firebase/auth/hooks.js
@@ -1,5 +1,9 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { doc, setDoc } from 'firebase/firestore'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+
+import { db } from '../db'
 
 import { AuthContext } from './provider'
 
@@ -13,6 +17,11 @@ export function useUser() {
 	return useAuthData().user
 }
 
+// useUserId gives the ID of the user.
+export function useUserId() {
+	return useUser()?.uid
+}
+
 // useSignedInCheck will check if the user is signed in. If not, it will be redirected to the given page.
 export function useSignedInCheck(redirectPage = '/') {
 	const navigate = useNavigate()
@@ -23,4 +32,32 @@ export function useSignedInCheck(redirectPage = '/') {
 		if (!loading && !user)
 			navigate(redirectPage)
 	}, [user, loading, navigate, redirectPage])
+}
+
+// useUserData gives the object with the user data of the user.
+export function useUserData() {
+	// Get the userId. Use a random string for request if no user is present so as not to confuse the hook call order.
+	const userId = useUserId() || 'noUserPresentYet'
+
+	// Load the data.
+	const [data, loading, error] = useDocumentData(doc(db, 'userData', userId))
+
+	// When no data is available yet, return undefined.
+	if (loading || error)
+		return undefined
+
+	// When the data is ready, return it.
+	return data || {}
+}
+
+// useSetUserData gives a setUserData function that allows you to update userData.
+export function useSetUserData() {
+	const userId = useUserId()
+	return useCallback((data) => {
+		if (!userId)
+			throw new Error(`Invalid setUserData call: no user is known at the moment. Check if the user is signed in before making a setUserData call.`)
+		if (data.constructor !== Object)
+			throw new Error(`Invalid user data: a request was made to set user data, but this request must be provided with a basic object. Received something of type "${data}".`)
+		return setDoc(doc(db, 'userData', userId), data, { merge: true })
+	}, [userId])
 }

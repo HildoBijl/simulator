49363e1dacafffd1313496eaf8ac2742074be47c
Hildo Bijl <hildobijl@gmail.com>
2024-03-15 09:47:43 +0100

    Set up simulation overview table

--- src/pages/simulations/Create.jsx
+++ src/pages/simulations/Create.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import clsx from 'clsx'
+import { styled } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 
 import { useAuthData, useUserId, signInWithGoogleRedirect } from '../../firebase'
@@ -25,8 +27,77 @@ export function CreateAsStranger() {
   </CreatePage>
 }
 
+const Grid = styled('div')({
+  display: 'grid',
+  margin: '1rem 0',
+  gridTemplateColumns: '[start] 3fr repeat(2, minmax(140px, 1fr)) [end]',
+
+  '& > div': {
+    lineHeight: '1.5rem',
+    padding: '0.6rem 0.2rem',
+  },
+
+  '& .head': {
+    fontWeight: 'bold',
+  },
+  '& .row': {
+    borderTop: '1px solid #555555',
+    '@media (prefers-color-scheme: light)': {
+      borderTop: '1px solid #cccccc',
+    },
+    cursor: 'pointer',
+  },
+  '& .hovering': {
+    backgroundColor: '#2c2c2c',
+    '@media (prefers-color-scheme: light)': {
+      background: '#f2f1f0',
+    },
+  },
+
+  '& .title': {
+
+  },
+  '& .numPlayed': {
+    textAlign: 'center',
+  },
+  '& .numFinished': {
+    textAlign: 'center',
+  },
+
+  '& .newGame': {
+    borderTop: '1px solid #555555',
+    '@media (prefers-color-scheme: light)': {
+      borderTop: '1px solid #cccccc',
+    },
+    cursor: 'pointer',
+    fontSize: '2em',
+    gridColumn: 'start / end',
+    textAlign: 'center',
+    '& > span': {
+      display: 'inline-block',
+      transform: 'translateY(-3px)',
+    },
+    
+    '&:hover': {
+      backgroundColor: '#2c2c2c',
+      '@media (prefers-color-scheme: light)': {
+        background: '#f2f1f0',
+      },
+    },
+
+    '&.addingGame': {
+      backgroundColor: '#353535',
+      '@media (prefers-color-scheme: light)': {
+        background: '#e7e6e5',
+      },
+    },
+  }
+})
+
 export function CreateAsUser() {
   const simulationIds = useSimulationIds()
+  const [hoverRow, setHoverRow] = useState()
+  const navigate = useNavigate()
 
   // When no data is available, show a loading note.
   if (!simulationIds)
@@ -35,15 +106,23 @@ export function CreateAsUser() {
   // Render the component.
   return (
     <CreatePage>
-      {simulationIds.map(simulationId => <Simulation key={simulationId} id={simulationId} />)}
-      <NewSimulation />
+      <Grid>
+        <div className="title head">Titel der Simulation</div>
+        <div className="numPlayed head">Angefangen</div>
+        <div className="numFinished head">Abgeschlossen</div>
+        {simulationIds.map(simulationId => <Simulation key={simulationId} id={simulationId} onClick={() => navigate(`/create/${simulationId}`)} hovering={hoverRow === simulationId} startHover={() => setHoverRow(simulationId)} endHover={() => setHoverRow()} />)}
+        <NewSimulation />
+      </Grid>
     </CreatePage>
   )
 }
 
-function Simulation({ id }) {
-  const navigate = useNavigate()
-  return <div onClick={() => navigate(`/create/${id}`)}>{id}</div>
+function Simulation({ id, hovering, startHover, endHover, onClick }) {
+  return <>
+    <div className={clsx('title', 'row', { hovering })} onClick={onClick} onMouseOver={startHover} onMouseOut={endHover}>{id}</div>
+    <div className={clsx('numPlayed', 'row', { hovering })} onClick={onClick} onMouseOver={startHover} onMouseOut={endHover}>0</div>
+    <div className={clsx('numFinished', 'row', { hovering })} onClick={onClick} onMouseOver={startHover} onMouseOut={endHover}>0</div>
+  </>
 }
 
 function NewSimulation() {
@@ -60,5 +139,5 @@ function NewSimulation() {
   }
 
   // Render the button to add a new simulation.
-  return <button onClick={newGame}>{!addingGame ? 'New game' : 'Adding game...'}</button>
+  return <div className={clsx('newGame', { addingGame })} onClick={newGame}><span>+</span></div>
 }

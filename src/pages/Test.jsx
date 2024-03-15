262f51851011295cf7abfcd5884e466cc968c655
Hildo Bijl <hildobijl@gmail.com>
2024-03-15 10:55:39 +0100

    Clean up Test page

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,41 +1,9 @@
-import { collection, doc, setDoc } from 'firebase/firestore'
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { useNavigate } from 'react-router-dom'
-
-import { db } from '../firebase'
 import { Page } from '../components'
 
 export function Test() {
-  // Load in navigation.
-  const navigate = useNavigate()
-
-  // Load game data.
-  const [value, loading, error] = useCollection(collection(db, 'games'))
-
-  // Allow play count increment.
-  const increment = async (id) => {
-    const oldData = value.docs.find(doc => doc.id === id).data()
-    await setDoc(doc(db, 'games', id), { playCount: oldData.playCount + 1 }, { merge: true })
-  }
-
-  // Render the component.
-  if (!value || loading || error)
-    return null
   return (
-    <Page addAppBar={false}>
-      <h1>Führungssimulator</h1>
-      <p>Welcome student! You can play the games below.</p>
-      {value.docs.map(doc => {
-        return <div className="card" key={doc.id}>
-          <p>Simulation title is <strong>{doc.data().name}</strong>. Number of plays: <strong>{doc.data().playCount}</strong>.</p>
-          <button onClick={() => increment(doc.id)}>
-            Play this game!
-          </button>
-        </div>
-      })}
-      <h2>Create new simulation</h2>
-      <p>Are you a teacher? Then you can build your own simulation.</p>
-      <button onClick={() => navigate('/create')}>Create new simulation</button>
+    <Page title="Test page" backButton="/create">
+      <p>This is a test page. It will often have random contents used for testing purposes.</p>
     </Page>
   )
 }

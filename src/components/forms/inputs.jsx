1a601174a14ec0bc399ea10e34dfdb0633061fc6
Hildo Bijl <hildobijl@gmail.com>
2024-04-01 11:13:06 +0200

    Prevent cursor jumps upon typing in input fields

--- src/components/forms/inputs.jsx
+++ src/components/forms/inputs.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react'
 import TextField from '@mui/material/TextField'
 
 import { updateDocument } from '../../firebase'
@@ -5,7 +6,21 @@ import { updateDocument } from '../../firebase'
 import { FormPart } from './containers'
 
 export function TrackedTextField({ path, documentId, field, label, value, multiline }) {
+	// Set up a tracking system for the cursor position, so that it doesn't jump upon changes.
+	const [cursor, setCursor] = useState(null)
+	const ref = useRef(null)
+	useEffect(() => {
+		const field = ref.current
+		if (field)
+			field.setSelectionRange(cursor, cursor)
+	}, [ref, cursor, value])
+	const handleChange = (event) => {
+		setCursor(event.target.selectionStart)
+		updateDocument(path, documentId, { [field]: event.target.value })
+	}
+
+	// Render the form.
 	return <FormPart>
-		<TextField variant="outlined" fullWidth multiline={multiline} label={label} value={value || ''} onChange={(event) => updateDocument(path, documentId, { [field]: event.target.value })} />
+		<TextField inputRef={ref} variant="outlined" fullWidth multiline={multiline} label={label} value={value || ''} onChange={handleChange} />
 	</FormPart>
 }

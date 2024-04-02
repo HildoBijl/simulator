6e5518c67236d3774e4caf0c39423846cd1747bd
Hildo Bijl <hildobijl@gmail.com>
2024-04-02 10:39:27 +0200

    Fix the text fields to not have a delay upon typing

--- src/components/forms/inputs.jsx
+++ src/components/forms/inputs.jsx
@@ -1,22 +1,18 @@
-import { useState, useEffect, useRef } from 'react'
 import TextField from '@mui/material/TextField'
 
+import { useTrackedState } from '../../util'
 import { updateDocument } from '../../firebase'
 
 import { FormPart } from './containers'
 
-export function TrackedTextField({ path, documentId, field, label, value, arrayValue = [], arrayIndex, arrayField, multiline, ...otherProps }) {
-	// Set up a tracking system for the cursor position, so that it doesn't jump upon changes.
-	const [cursor, setCursor] = useState(null)
-	const ref = useRef(null)
-	useEffect(() => {
-		const field = ref.current
-		if (field)
-			field.setSelectionRange(cursor, cursor)
-	}, [ref, cursor, value])
+export function TrackedTextField({ path, documentId, field, label, value: givenValue, arrayValue = [], arrayIndex, arrayField, multiline, ...otherProps }) {
+	// Track the given state to also update on external changes.
+	const [value, setValue] = useTrackedState(givenValue)
+
+	// Save any changes locally and then in the database.
 	const handleChange = (event) => {
-		setCursor(event.target.selectionStart)
 		const newValue = event.target.value
+		setValue(newValue)
 		if (arrayIndex !== undefined) { // Do we change a part of an array?
 			if (arrayField) { // The array contains objects and we need to change an element in the array.
 				updateDocument(path, documentId, { [field]: [...arrayValue.slice(0, arrayIndex), { ...arrayValue[arrayIndex], [arrayField]: newValue }, ...arrayValue.slice(arrayIndex + 1)] })
@@ -30,6 +26,6 @@ export function TrackedTextField({ path, documentId, field, label, value, arrayV
 
 	// Render the form.
 	return <FormPart>
-		<TextField inputRef={ref} variant="outlined" fullWidth multiline={multiline} label={label} value={value || ''} onChange={handleChange} {...otherProps} />
+		<TextField variant="outlined" fullWidth multiline={multiline} label={label} value={value || ''} onChange={handleChange} {...otherProps} />
 	</FormPart>
 }

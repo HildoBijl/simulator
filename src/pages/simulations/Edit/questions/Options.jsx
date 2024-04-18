8b1085a04481b80ad91da45922e3cedfdf572d8a
Hildo Bijl <hildobijl@gmail.com>
2024-04-18 10:32:45 +0200

    Fix minor style issue in light mode

--- src/pages/simulations/Edit/questions/Options.jsx
+++ src/pages/simulations/Edit/questions/Options.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import Accordion from '@mui/material/Accordion'
 import AccordionActions from '@mui/material/AccordionActions'
 import AccordionDetails from '@mui/material/AccordionDetails'
@@ -37,8 +38,9 @@ export function Options({ simulation, question, index: questionIndex }) {
 	}
 
 	// Render the options through an Accordion.
+	const prefersLight = useMediaQuery('(prefers-color-scheme: light)')
 	return <>
-		<h5 style={{ color: 'rgba(255,255,255,0.7)', '@media (prefersColorScheme: light)': { color: 'rgba(0,0,0,0.6)' }, fontSize: '0.9rem', fontWeight: 400, margin: '-0.5rem 0 0 0.4rem' }}>Antwortmöglichkeiten</h5>
+		<h5 style={{ color: prefersLight ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.7)', fontSize: '0.9rem', fontWeight: 400, margin: '-0.5rem 0 0 0.4rem' }}>Antwortmöglichkeiten</h5>
 		<div>
 			{options.map((option, optionIndex) => <Option key={optionIndex} {...{ simulation, question, questionIndex, optionIndex, expanded: !!expanded[optionIndex], flipExpand: () => flipExpand(optionIndex), removeOption: () => removeOption(optionIndex) }} />)}
 			{canAddOption ? <Accordion onClick={() => addOption()} expanded={false}>

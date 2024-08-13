86cca008a1bd21f39b7e5db0fe8cc1343a4f966a
Hildo Bijl <hildobijl@gmail.com>
2024-08-13 12:29:06 +0200

    Rename 'start simulation' button to 'start'

--- src/pages/simulations/Simulation/subpages/StartPage.jsx
+++ src/pages/simulations/Simulation/subpages/StartPage.jsx
@@ -8,6 +8,6 @@ export function StartPage({ simulation, start }) {
 	return <Page title={simulation.title || '[Simulationstitel fehlt]'}>
 		<MCEContents>{simulation.description || '<p>[Diese Simulation hat noch keine Beschreibung.]</p>'}</MCEContents>
 		<Media media={simulation.media} />
-		{simulation.questions ? <Button variant="contained" sx={{ margin: '0 0 1rem 0' }} onClick={() => start(isOwner)}>Simulation anfangen</Button> : <p>Diese Simulation kann noch nicht gespielt werden: Es sind noch keine Fragen vorhanden.</p>}
+		{simulation.questions ? <Button variant="contained" sx={{ margin: '0 0 1rem 0' }} onClick={() => start(isOwner)}>Anfangen</Button> : <p>Diese Simulation kann noch nicht gespielt werden: Es sind noch keine Fragen vorhanden.</p>}
 	</Page>
 }

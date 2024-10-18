b572a02323a10f87a97bf9b7a1aff2b87c9c2a39
Hildo Bijl <hildobijl@gmail.com>
2024-10-18 14:21:57 +0200

    Add dummy image library component

---
+++ src/pages/simulations/Edit/settings/ImageLibrary.jsx
@@ -0,0 +1,7 @@
+export function ImageLibrary({ simulation }) {
+	return <>
+		<h2>Bilddatenbank</h2>
+		<p>Sie können Bilder über ihre URL zu Simulationsseiten hinzufügen. Das Hinzufügen von Bildern, die bereits online sind, ist daher einfach. Ist Ihr Bild noch nicht online? Dann laden Sie es hier in Ihre eigene Bildbibliothek hoch, um eine URL zu erhalten.</p>
+		<p>Diese Option befindet sich noch im Aufbau. Schauen Sie in einer Woche wieder vorbei.</p>
+	</>
+}
--- src/pages/simulations/Edit/settings/Settings.jsx
+++ src/pages/simulations/Edit/settings/Settings.jsx
@@ -1,5 +1,6 @@
 import { MainSettings } from './MainSettings'
 import { OtherSettings } from './OtherSettings'
+import { ImageLibrary } from './ImageLibrary'
 import { Ownership } from './Ownership'
 import { RemoveSimulation } from './RemoveSimulation'
 
@@ -7,6 +8,7 @@ export function Settings({ simulation }) {
 	return <>
 		<MainSettings {...{ simulation }} />
 		<OtherSettings {...{ simulation }} />
+		<ImageLibrary {...{simulation}} />
 		<Ownership {...{ simulation }} />
 		<RemoveSimulation {...{ simulation }} />
 	</>
